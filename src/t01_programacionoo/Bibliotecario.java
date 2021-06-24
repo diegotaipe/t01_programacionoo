1660f36ddc548d395583b36bc001c3091f465f7f
diego <diego@LAPTOP-7QUSVIB8>
2021-06-23 20:07:37 -0500

    a

--- src/t01_programacionoo/Bibliotecario.java
+++ src/t01_programacionoo/Bibliotecario.java
@@ -1,5 +1,12 @@
 package t01_programacionoo;
 
 public class Bibliotecario {
-
+	/*Encapsulación*/
+	private String nombre;
+	private String facultad;
+	
+	/*Abstracción*/
+	void correr() {
+		int velocidad=0;
+	}
 }
