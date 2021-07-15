629b1c23e7be1bd4381bc074bbef8e02ad54f23d
diego <diego@LAPTOP-7QUSVIB8>
2021-07-14 19:09:51 -0500

    figura

---
+++ src/diagramas/de/flujo/MainFigura1.java
@@ -0,0 +1,23 @@
+package diagramas.de.flujo;
+
+import java.util.Scanner;
+
+public class MainFigura1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Ingrese el número de filas: ");
+
+		Scanner lector = new Scanner(System.in);
+		int n = lector.nextInt();
+
+		for (int fila = 1; fila <= n; fila = fila + 1) {
+
+			for (int ast = 1; ast <= fila; ast ++) {
+				System.out.print("*");
+			}
+			System.out.println();//Salto de linea
+		}
+	}
+
+}
---
+++ src/diagramas/de/flujo/MainFigura2.java
@@ -0,0 +1,22 @@
+package diagramas.de.flujo;
+
+import java.util.Scanner;
+
+public class MainFigura2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("Ingrese el número de filas: ");
+
+Scanner lector=new Scanner(System.in);
+int n = lector.nextInt();
+for (int fila = 1; fila <n ; fila = fila + 1) {
+
+	for (int ast = n; ast <= fila; ast ++) {
+		System.out.print("*");
+	}
+	System.out.println();//Salto de linea
+}
+	}
+
+}
