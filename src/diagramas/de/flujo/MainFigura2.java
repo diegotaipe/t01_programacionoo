04ea3d6a125b50a0f2199fcdd8d6d02497af72c9
diego <diego@LAPTOP-7QUSVIB8>
2021-07-18 12:26:13 -0500

    Taller GitHub

--- src/diagramas/de/flujo/MainFigura2.java
+++ src/diagramas/de/flujo/MainFigura2.java
@@ -6,17 +6,17 @@ public class MainFigura2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.print("Ingrese el número de filas: ");
+		System.out.print("Ingrese el número de filas: ");
 
-Scanner lector=new Scanner(System.in);
-int n = lector.nextInt();
-or (int fila = 1; fila <= n; fila ++) {
-	for (int ast = n; ast >= fila; ast--) {
-		System.out.print("*");
-	}
-	System.out.println();// Salto de línea
-	 
-} 
+		Scanner lector = new Scanner(System.in);
+		int n = lector.nextInt();
+		for (int fila = 1; fila <= n; fila++) {
+			for (int ast = n; ast >= fila; ast--) {
+				System.out.print("*");
+			}
+			System.out.println();// Salto de línea
+
+		}
 	}
 
 }
