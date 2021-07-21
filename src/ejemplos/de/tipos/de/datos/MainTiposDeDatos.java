c00635e39a50c246200450248ff4c3b5ac6ae3c6
diego <diego@LAPTOP-7QUSVIB8>
2021-07-21 18:48:50 -0500

    Taller

---
+++ src/ejemplos/de/tipos/de/datos/MainTiposDeDatos.java
@@ -0,0 +1,17 @@
+package ejemplos.de.tipos.de.datos;
+
+public class MainTiposDeDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short cantidadEstudiantes=1000;
+		byte entradaCircuito=0;
+		int cantCiudadanos=100000;
+		long cantidadMundo=1000000000;
+		float sueldo = 1200.56f;
+		double saldoBanco=10000000.75;
+		char opcionMenu='C';
+		boolean casado = true;
+	}
+
+}
