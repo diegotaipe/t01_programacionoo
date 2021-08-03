f29108fb0f1cfe918e3e75d131eabf3d0e658831
diego <diego@LAPTOP-7QUSVIB8>
2021-08-02 20:04:03 -0500

    Taller 2/08/2021

---
+++ src/ejercicios/matrices/MainMatrices.java
@@ -0,0 +1,27 @@
+package ejercicios.matrices;
+
+public class MainMatrices {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1. Declarar una matriz A de tipo char con 5 filas y 4 columnas.
+		char A[][] = new char[5][4];
+
+		// 2. Declarar una matriz B de tipo char con 5 filas y 6 columnas.
+		char B[][] = new char[5][6];
+
+		// 3. Declarar una matriz miMatriz de tipo long con 2 filas y 4 columnas.
+		long miMatriz[][] = new long[2][4];
+
+		// 4. Declarar un matriz miMatriz1 de tipo long con 1 filas y 2 columnas. Y
+		// asignar a la posición [0][0] el valor de 2, a la posición [1][1] el valor de
+		// 8, a la posición [2][2] el valor de 2.
+
+		long miMatriz1[][] = new long[1][2];
+		miMatriz1[0][0] = 2;
+		miMatriz1[1][1] = 8;
+		miMatriz1[2][2] = 2;
+
+	}
+
+}
