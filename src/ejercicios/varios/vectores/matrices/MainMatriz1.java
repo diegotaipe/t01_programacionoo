55d14906377af3f874f905fdbd926c489d88d287
diego <diego@LAPTOP-7QUSVIB8>
2021-08-04 19:30:13 -0500

    Taller en clase 4. Matrices

---
+++ src/ejercicios/varios/vectores/matrices/MainMatriz1.java
@@ -0,0 +1,41 @@
+package ejercicios.varios.vectores.matrices;
+
+import java.util.Scanner;
+
+public class MainMatriz1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1. Declare una matriz A [3,4] de tipo char, y desarrolle un programa que vaya
+		// solicitando cada una de sus posiciones
+		Scanner lector = new Scanner(System.in); // este es mi teclado
+
+		System.out.print("Ingrese el número de filas del vector: ");
+		int longitudFila = lector.nextInt();
+
+		System.out.print("Ingrese el número de columnas del vector: ");
+		int longitudColumna = lector.nextInt();
+
+		char A[][] = new char[longitudFila][longitudColumna];
+
+		for (int fila = 0; fila < longitudFila; fila++) {
+			for (int columna = 0; columna < longitudColumna; columna++) {
+				System.out.print("Ingrese el valor fila [" + fila + "] y columna [" + columna + "]: ");
+				char n = lector.next().charAt(0);
+
+				A[fila][columna] = n;
+			}
+
+		}
+		for (int fila = 0; fila < longitudFila; fila++) {
+			for (int columna = 0; columna < longitudColumna; columna++) {
+
+				System.out.println(
+						"El valor de la fila: [" + fila + "] y columna: [" + columna + "] es: " + A[fila][columna]);
+
+			}
+
+		}
+	}
+
+}
