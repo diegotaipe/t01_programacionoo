2b9bde9c2a83f39e2d33e6e77dd02a244e33b4ff
Diego Taipe <diegotaipe@outlook.com>
2021-08-25 20:01:18 -0500

    Taller en clase 10. nuevo acceso token

---
+++ src/ejercicios/error/git/MainToken.java
@@ -0,0 +1,11 @@
+package ejercicios.error.git;
+
+public class MainToken {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Taller token gitHub
+		//Captura en Git donde se visualice la clase y su nombre
+	}
+
+}
