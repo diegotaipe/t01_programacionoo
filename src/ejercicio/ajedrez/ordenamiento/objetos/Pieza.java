8787f4d4260f544a88d237714e0009b10c2bc5ef
Diego Taipe <diegotaipe@outlook.com>
2021-08-27 20:05:28 -0500

    Taller en clase 11

--- src/ejercicio/ajedrez/ordenamiento/objetos/Pieza.java
+++ src/ejercicio/ajedrez/ordenamiento/objetos/Pieza.java
@@ -1,19 +1,5 @@
 package ejercicio.ajedrez.ordenamiento.objetos;
 
-public class Pieza {
-	private String color;
-
-	public String getColor() {
-		return color;
-	}
-
-	public void setColor(String color) {
-		this.color = color;
-	}
-
-	@Override
-	public String toString() {
-		return "Pieza [color=" + color + "]";
-	}
-
+public abstract class Pieza {
+	
 }
